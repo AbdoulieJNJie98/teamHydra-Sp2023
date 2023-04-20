ce1fc2b21865bcb366a11db0bf1796cc1d765ae7
jasonurquilla <jurquilla@ggc.edu>
2023-04-19 20:30:41 -0400

    Added monster object to various classes including arraylist and hashmaps of them

--- teamHydra-2023/src/main/java/Map.java
+++ teamHydra-2023/src/main/java/Map.java
@@ -32,16 +32,18 @@ public class Map  implements Serializable {
     public String roomsFilePath = "rooms.txt";
     public String itemsFilePath = "Items.txt";
     public String puzzlesFilePath = "Puzzles.txt";
+    public String monstersFilePath = "monster.txt";
     // hash map used to store and retrieve the elements read from file
     public HashMap<Integer, Rooms> hashMapRooms = new HashMap<Integer, Rooms>();
     public HashMap<Integer, Items> hashMapItems = new HashMap<Integer, Items>();
     public HashMap<Integer, Puzzles> hashMapPuzzles = new HashMap<Integer, Puzzles>();
-
+    public HashMap<Integer, Monster> hashMapMonster = new HashMap<Integer, Monster>();
 
     // array list of items, rooms, and puzzles used throughout the game
     public ArrayList<Items> arrayListOfItems = new ArrayList<>();
     public ArrayList<Puzzles> arrayListOfPuzzles = new ArrayList<>();
     public ArrayList<Rooms> arrayListOfRooms = new ArrayList<>();
+    public ArrayList<Monster> arrayListOfMonsters = new ArrayList<>();
     public Map() {
         readFiles();
     } // method used to read the room files
@@ -60,7 +62,7 @@ public class Map  implements Serializable {
                 if (parts.length >= 10) {
                     // temp room object used to hold information collected from current iteration of loop
                     Rooms r = new Rooms(Integer.parseInt(parts[0]), Boolean.parseBoolean(parts[1]), parts[2], Integer.parseInt(parts[3]), Integer.parseInt(parts[4]),
-                            Integer.parseInt(parts[5]), Integer.parseInt(parts[6]), parts[7], Integer.parseInt(parts[8]), Integer.parseInt(parts[9]));
+                            Integer.parseInt(parts[5]), Integer.parseInt(parts[6]), parts[7], Integer.parseInt(parts[8]), Integer.parseInt(parts[9]), Integer.parseInt(parts[10]));
                     hashMapRooms.put(r.getRoomID(), r); // used to add the values stored in the temp room to the hash map
                     arrayListOfRooms.add(r);
                 }
@@ -106,6 +108,28 @@ public class Map  implements Serializable {
                     // temp room object used to hold information collected from current iteration of loop
                     Puzzles p = new Puzzles(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]),parts[2], parts[3], parts[4], parts[5], parts[6]);
                     hashMapPuzzles.put(p.getPuzzleID(), p); // used to add the values stored in the temp puzzle to the hash map
+                }
+
+            }
+            bufferedReader.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        // try catch statement that uses a buffered reader to read the text file, split data in the text file based on the commas found within it,
+        // and stores the information in a hash map, or prints a IO Exception to indicate an issue with reading in the desired file.
+        try {
+            String line;
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(monstersFilePath)); // buffer reader used to read the file
+            while ((line = bufferedReader.readLine()) != null) { // while loop that reads until the end of the file
+
+                String[] parts = line.split("~", 6); // String array used to each element in the given file individually
+                if (parts.length >= 6) {
+                    // temp item object used to hold information collected from current iteration of loop
+                    Monster m = new Monster(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3]), parts[4], parts[5]);
+                    hashMapMonster.put(m.getMonsterID(), m); // used to add the values stored in the temp items to the hash map
+                    arrayListOfMonsters.add(m);
+
                 }
 
             }
