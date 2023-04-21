ccadbeb05773c1908f0273a5888af1bebc11bd60
Barbara Yeboah <byeboah@ggc.edu>
2023-04-20 21:09:33 -0400

    Created Help method

--- teamHydra-2023/src/main/java/Exhibit.java
+++ teamHydra-2023/src/main/java/Exhibit.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
     The exhibit class is used to hold the array list of items the player has archived, as well as display the exhibit menu
@@ -19,7 +20,7 @@ public class Exhibit implements Serializable {
         System.out.println("Exhibit");
         System.out.println("----------------------");
         for (int i = 0; i < itemsInExhibit.size(); i++) {
-            System.out.println(itemsInExhibit.get(i));
+            System.out.println(itemsInExhibit.get(i).getItemName());
         }
         System.out.println("Here are the treasure items that have been archived in the museum.");
 
@@ -27,6 +28,19 @@ public class Exhibit implements Serializable {
     }
     public void Help() {
         System.out.println("If you want to display the treasure items that have been archived in the museum, type 'Exhibit'.");
+        System.out.println("If you want to display the description of an item, type the name of the item and 'Description'.");
+        Scanner input = new Scanner(System.in);
+        String help = input.nextLine();
+        String d = "Description";
+        int searchIndex = help.indexOf(d);
+        if (help.contains(d) && itemsInExhibit.size() > 0) {
+            String result = help.substring(0, searchIndex);
+            for (int i = 0; i < itemsInExhibit.size(); i++) {
+                if (result.equals(itemsInExhibit.get(i).getItemName())) {
+                    System.out.println("The description of " + itemsInExhibit.get(i).getItemName() + " is: " + itemsInExhibit.get(i).getItemDescription());
+                }
+            }
+        }
     }
 
     // method used to get array list of items in the exhibit
